b8f757dc093286bdb7790d4fd66a7488a7b8fcdf
Karl Mouchantaf <mouchantafkarl@gmail.com>
2024-10-09 18:08:48 +0200

    added country to frontend

--- src/components/AppAccounts.vue
+++ src/components/AppAccounts.vue
@@ -27,6 +27,7 @@
                 <th scope="col">Account Number</th>
                 <th scope="col">Account Balance</th>
                 <th scope="col">Account Currency</th>
+                <th scope="col">Account Country</th>
                 <th scope="col">Account Status</th>
                 <th scope="col">Actions</th>
               </tr>
@@ -37,6 +38,7 @@
                 <td>{{ account.account_number }}</td>
                 <td>{{ account.balance }}</td>
                 <td>{{ account.currency }}</td>
+                <td>{{ account.country }}</td>
                 <td>
                   <span
                     v-if="account.status == 'Active'"
@@ -110,7 +112,20 @@
             >
             </b-form-input>
           </b-form-group>
-
+          <b-form-group
+            id="form-currency-group"
+            label="Country:"
+            label-for="form-currency-input"
+          >
+            <b-form-input
+              id="form-country-input"
+              type="text"
+              v-model="createAccountForm.country"
+              placeholder="Country"
+              required
+            >
+            </b-form-input>
+          </b-form-group>
           <b-button type="submit" variant="outline-info">Submit</b-button>
         </b-form>
       </b-modal>
@@ -156,6 +171,7 @@ export default {
       createAccountForm: {
         name: "",
         currency: "",
+        country: "",
       },
       editAccountForm: {
         id: "",
@@ -257,6 +273,7 @@ export default {
     initForm() {
       this.createAccountForm.name = "";
       this.createAccountForm.currency = "";
+      this.createAccountForm.country = "";
       this.editAccountForm.id = "";
       this.editAccountForm.name = "";
     },
@@ -268,6 +285,7 @@ export default {
       const payload = {
         name: this.createAccountForm.name,
         currency: this.createAccountForm.currency,
+        country: this.createAccountForm.country,
       };
       this.RESTcreateAccount(payload);
       this.initForm();
---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="homepage">
+      <h1>Welcome to the IE Bank Corp.</h1>
+      <div class="links">
+          <router-link to="/accounts" class="link-button">Accounts</router-link>
+          <router-link to="/skull" class="link-button">Skull</router-link>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "HomePage",
+}
+</script>
+<style scoped>
+.homepage {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+}
+h1 {
+    font-size: 72px;
+    background: -webkit-linear-gradient(left, #fb0000, #eaad07);
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: transparent; /* Fallback for non-webkit browsers */
+}
+p {
+    color: #0e0630;
+}
+.links {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    gap: 0 20px;
+}
+</style>
